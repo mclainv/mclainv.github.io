020b9f9bc424b0dab8daa6d55333a384cba412cd
McLain <rma9540@nyu.edu>
2025-01-14 20:53:11 -0500

    added scroll link to jailbreak section

--- scripts.js
+++ scripts.js
@@ -1,5 +1,13 @@
-const scroller = document.getElementById('portfolio')
+window.addEventListener("DOMContentLoaded", (event) => {
+  document.getElementById('jailbreak-landing-button').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the default link behavior
+    document.getElementById('jailbreak-section').classList.toggle('active');
+    document.body.classList.toggle('active');
+    document.getElementById('jailbreak-section').scrollIntoView({behavior: 'smooth'});
+  });
+});
+// const scroller = document.getElementById('portfolio');
 
-window.addEventListener('wheel', e => {
-  scroller.scrollLeft += e.deltaY;
-})
+// window.addEventListener('wheel', e => {
+//   scroller.scrollLeft += e.deltaY;
+// })
